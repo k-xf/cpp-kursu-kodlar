19f7dfd8cc42912d55ad1d0f1d1810a9c91c433f
Necati Ergin <46518309+necatiergin@users.noreply.github.com>
2022-03-27 08:02:34 +0300

    Update bad_function_call_01.cpp

--- STL/functional/function/bad_function_call_01.cpp
+++ STL/functional/function/bad_function_call_01.cpp
@@ -1,22 +1,19 @@
+// boş bir std::function nesnesi ile çağrı yaparsak
+// std::bad_function_call türünden exception throw eder.
+
 #include <functional>
 #include <iostream>
 
-
 int main()
 {
-	using namespace std;
-
-	function<int(int)> fnc;
+	std::function<int(int)> fnc;
 
 	try {
 		auto x = fnc(13);
-
 	}
 	//catch (const std::exception& ex) {
 	catch (const std::bad_function_call& ex) {
 		std::cout << "hata yakalandi : " << ex.what() << "\n";
-		cout << typeid(ex).name() << "\n";
+		std::cout << typeid(ex).name() << "\n";
 	}
-
-
 }
