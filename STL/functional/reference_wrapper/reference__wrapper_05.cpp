63e9615f9dac174752774c33a89ff439850ea053
Necati Ergin <46518309+necatiergin@users.noreply.github.com>
2022-03-27 07:49:24 +0300

    Update reference__wrapper_05.cpp

--- STL/functional/reference_wrapper/reference__wrapper_05.cpp
+++ STL/functional/reference_wrapper/reference__wrapper_05.cpp
@@ -10,12 +10,14 @@
 int main()
 {
 	using namespace std;
-	
-	list<string> slist{ "hasan", "demir", "nur", "kaya", "berk"};
+
+	list<string> slist{ "hasan", "demir", "nur", "kaya", "berk" };
 	vector<reference_wrapper<string>> vec{ slist.begin(), slist.end() };
 	shuffle(vec.begin(), vec.end(), mt19937{ random_device{}() });
 	copy(vec.begin(), vec.end(), ostream_iterator<string>{cout, " "});
-
+	
+	cout << '\n';
+	
 	for (auto& s : vec) {
 		s.get() += "can";
 	}
