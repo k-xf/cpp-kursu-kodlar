19e35f6326be15305ac6625036f42148c9162812
Necati Ergin <46518309+necatiergin@users.noreply.github.com>
2022-04-10 07:45:33 +0300

    Update and rename STL/unique_ptr/custom_deleter_02.cpp to STL/unique_ptr/deleter/custom_deleter_02.cpp

---
+++ STL/unique_ptr/deleter/custom_deleter_02.cpp
@@ -0,0 +1,16 @@
+#include <iostream>
+#include <memory>
+#include <string>
+
+int main()
+{
+	using namespace std;
+
+	auto f = [](string* p) {
+		cout << "lambda deleter deletes the string at the address " << p << "\n";
+		delete p;
+	};
+
+	//unique_ptr<string, decltype(f)> uptr{ new string{"Necati"}, f };
+	unique_ptr<string, decltype(f)> uptr{ new string{"Necati"} }; //C++20
+}
