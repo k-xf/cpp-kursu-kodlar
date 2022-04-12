9758dc79ae36534529720f634e63840a4d5f2ebc
Necati Ergin <46518309+necatiergin@users.noreply.github.com>
2022-04-12 09:11:08 +0300

    Create agg5.cpp

---
+++ cpp20/aggregates/agg5.cpp
@@ -0,0 +1,24 @@
+#include <iostream>
+
+struct Nec {
+	int a, b, c;
+};
+
+int main()
+{
+	Nec n1{ 1,2,3 }; // Valid C++17/C++20
+	Nec n2(1, 2, 3); // Invalid at C++17, Valid at C++20.
+
+	//--------------------------------------------------------------------------------
+
+	Nec n3{ 1, 2.2, 3 }; // invalid : Possible loss of data (narrowing conversion)
+
+	Nec n4(1, 2.2, 3); // Warning : Possible loss of data (narrowing conversion)
+
+	////--------------------------------------------------------------------------------
+
+	Nec n5(.b = 6); // Invalid C++17/C++20
+
+	Nec n6{ .b = 6 }; // Valid at C++20. Invalid at C++17
+
+}
