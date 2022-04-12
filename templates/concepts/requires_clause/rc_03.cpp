7baa2f3628f08afd09df38df831f12f8b9eca498
Necati Ergin <46518309+necatiergin@users.noreply.github.com>
2022-04-12 09:42:29 +0300

    Create rc_03.cpp

---
+++ templates/concepts/requires_clause/rc_03.cpp
@@ -0,0 +1,20 @@
+#include <concepts>
+#include <iostream>
+
+template<std::integral T>
+class Tclass {
+
+};
+
+struct A {};
+
+template<typename T>
+concept Nec = requires {
+    typename Tclass<T>;
+};
+
+int main()
+{
+    static_assert(Nec<int>); // valid
+    static_assert(Nec<A>);  // invalid
+}
