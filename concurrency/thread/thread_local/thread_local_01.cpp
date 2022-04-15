f3d299f4dfc099093396e4778cfadd69db5c44e7
Necati Ergin <46518309+necatiergin@users.noreply.github.com>
2022-04-15 18:40:03 +0300

    Update thread_local_01.cpp

--- concurrency/thread/thread_local/thread_local_01.cpp
+++ concurrency/thread/thread_local/thread_local_01.cpp
@@ -1,16 +1,15 @@
 #include <iostream>
+#include <syncstream>
 #include <string>
 #include <thread>
 #include <mutex>
 
 thread_local int tval{ 0 };
-std::mutex cout_mutex;
 
 void func(const std::string& thread_name)
 {
     ++tval; //senkronizasyon gerekmiyor
-    std::lock_guard<std::mutex> lock(cout_mutex);
-    std::cout << "tval in thread " << thread_name << " is " << tval << "\n";
+    std::osyncstream{std::cout} << "tval in thread " << thread_name << " is " << tval << "\n";
 }
 
 int main()
@@ -19,8 +18,7 @@ int main()
     std::thread ty(func, "b");
 
     {
-        std::lock_guard<std::mutex> lock(cout_mutex);
-        std::cout << "tval in main: " << tval << '\n';
+        std::osyncstream{ std::cout } << "tval in main: " << tval << '\n';
     }
 
     tx.join();
