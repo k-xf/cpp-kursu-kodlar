507435ca979b2859af136f1eb695df138b785c59
Necati Ergin <46518309+necatiergin@users.noreply.github.com>
2022-04-15 14:38:04 +0300

    Update as_04.cpp

--- concurrency/async/as_04.cpp
+++ concurrency/async/as_04.cpp
@@ -1,60 +1,57 @@
-#include <iostream>
-#include <iomanip>
 #include <map>
 #include <string>
 #include <algorithm>
-#include <iterator>
+#include <iostream>
 #include <future>
-#include <cctype>
 
-using namespace std;
 
-static map<char, size_t> histogram(const string& s)
-{
-	map<char, size_t> cmap;
+namespace {
+	std::map<char, size_t> histogram(const std::string& s)
+	{
+		std::map<char, std::size_t> cmap;
 
-	for (char c : s)
-		++cmap[c];
+		for (char c : s)
+			++cmap[c];
 
-	return cmap;
+		return cmap;
+	}
+
+	std::string get_sorted(std::string str)
+	{
+		sort(begin(str), end(str));
+		erase_if(str, [](char c) {return isspace(c); });
+		return str;
+	}
+
+	bool is_vowel(char c)
+	{
+		static const char vowels[]{ "AEIOUaeiou" };
+		return std::find(std::begin(vowels), std::end(vowels), c) != std::end(vowels);
+	}
+
+	size_t count_vowels(const std::string& s)
+	{
+		return count_if(begin(s), end(s), is_vowel);
+	}
 }
-
-static string get_sorted(string str)
-{
-	sort(begin(str), end(str));
-	str.erase(remove_if(str.begin(), str.end(), [](char c) {return isspace(c); }), str.end());
-	return str;
-}
-
-static bool is_vowel(char c)
-{
-	static const char vowels[]{ "AEIOUaeiou" };
-	return find(begin(vowels), end(vowels), c) != end(vowels);
-}
-
-static size_t count_vowels(const string& s)
-{
-	return count_if(begin(s), end(s), is_vowel);
-}
-
 
 int main()
 {
-	string sline;
-	std::cout << "bir yazi girin: ";
+	std::string sline;
+	std::cout << "enter a string: ";
 
-	getline(cin, sline);
+	getline(std::cin, sline);
 
-	auto hist = async(launch::async, histogram, sline);
-	auto sorted_str = async(launch::async, get_sorted, sline);
-	auto vowel_count = async(launch::async, count_vowels, sline);
+	auto hist = async(std::launch::async, histogram, sline);
+	auto sorted_str = async(std::launch::async, get_sorted, sline);
+	auto vowel_count = async(std::launch::async, count_vowels, sline);
 
 	for (const auto& [c, count] : hist.get()) {
-		cout << c << ": " << count << '\n';
+		std::cout << c << ": " << count << '\n';
 	}
-	cout << "siralanmis yazi : "
+	
+	std::cout << "sorted string : "
 		<< quoted(sorted_str.get()) << '\n'
-		<< "toplam sesli harf : "
+		<< "total vowels: "
 		<< vowel_count.get() << '\n';
 }
-
