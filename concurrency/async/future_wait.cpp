04829e10610a0f58c42a274eb2a3d62155fd73e2
Necati Ergin <46518309+necatiergin@users.noreply.github.com>
2022-04-15 14:51:01 +0300

    Update future_wait.cpp

--- concurrency/async/future_wait.cpp
+++ concurrency/async/future_wait.cpp
@@ -1,33 +1,29 @@
-// örnek kod cpprefence.com sitesinden alınmıştır:
+// örnek kod cpprefence.com sitesinden alınıp degistirilmistir:
 
-    #include <chrono>
-    #include <iostream>
-    #include <future>
-    #include <thread>
+#include <chrono>
+#include <iostream>
+#include <future>
+#include <thread>
 
-    int fib(int n)
-    {
-        return (n < 3) ? 1 : fib(n - 1) + fib(n - 2);
-    }
+int fib(int n)
+{
+    return (n < 3) ? 1 : fib(n - 1) + fib(n - 2);
+}
 
-    int main()
-    {
-        std::future<int> f1 = std::async(std::launch::async, []() {
-            return fib(40);
-            });
-        std::future<int> f2 = std::async(std::launch::async, []() {
-            return fib(43);
-            });
+int main()
+{
+    auto ftr1 = async(std::launch::async, []() { return fib(40);});
+    auto ftr2 = async(std::launch::async, []() {return fib(43);});
 
-        std::cout << "waiting... " << std::flush;
-        const auto start = std::chrono::system_clock::now();
+    std::cout << "waiting... " << std::flush;
+    const auto start = std::chrono::system_clock::now();
 
-        f1.wait();
-        f2.wait();
+    ftr1.wait();
+    ftr2.wait();
 
-        const auto diff = std::chrono::system_clock::now() - start;
-        std::cout << std::chrono::duration<double>(diff).count() << " seconds\n";
+    const auto diff = std::chrono::system_clock::now() - start;
+    std::cout << std::chrono::duration<double>(diff).count() << " seconds\n";
 
-        std::cout << "f1: " << f1.get() << '\n';
-        std::cout << "f2: " << f2.get() << '\n';
-    }
+    std::cout << "ftr1: " << ftr1.get() << '\n';
+    std::cout << "ftr2: " << ftr2.get() << '\n';
+}
