b633209820f8191ba04e30699c203be2b4e2b612
Necati Ergin <46518309+necatiergin@users.noreply.github.com>
2022-04-16 07:34:23 +0300

    Update and rename shared_ptr_04.cpp to shared_ptr_constructors_01.cpp

--- STL/shared_ptr/shared_ptr_constructors_01.cpp
+++ STL/shared_ptr/shared_ptr_constructors_01.cpp
@@ -5,12 +5,11 @@ int main()
 {
 	using namespace std;
 
-	auto up = make_unique<string>("oguzhan katli");
-
-	shared_ptr<string> sp1;
-	shared_ptr<string> sp2{ nullptr };
+	shared_ptr<string> sp1; //default constructed - empty
+	shared_ptr<string> sp2{ nullptr }; //empty
 	shared_ptr<string> sp3{ new string {"ahmet ibrahim aksoy"} };
 	auto sp4 = make_shared<string>("omer ayyildiz");
+	auto up = make_unique<string>("oguzhan katli");
 	shared_ptr<string> sp5{ move(up) };
 	//...
 }
