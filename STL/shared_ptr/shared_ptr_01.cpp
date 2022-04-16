2e6a9c46b106e74d69abba873eba0ff6fdc27553
Necati Ergin <46518309+necatiergin@users.noreply.github.com>
2022-04-16 07:27:37 +0300

    Update shared_ptr_01.cpp

--- STL/shared_ptr/shared_ptr_01.cpp
+++ STL/shared_ptr/shared_ptr_01.cpp
@@ -2,23 +2,22 @@
 #include <iostream>
 #include "person.h"
 
-using namespace std;
-
-
 int main()
 {
+	using namespace std;
 	{
 		auto sp1 = make_shared<Person>("Necati", "Ergin");
 		cout << *sp1 << "\n";
 		cout << "sp1.use_count() = " << sp1.use_count() << "\n";
 		cout << "bir tusa basin ";
-		getchar();
+		(void)getchar();
 		{
 			auto sp2 = sp1;
 			cout << *sp2 << "\n";
+			cout << "sp1.use_count() = " << sp1.use_count() << "\n";
 			cout << "sp2.use_count() = " << sp2.use_count() << "\n";
 			cout << "bir tusa basin ";
-			getchar();
+			(void)getchar();
 			{
 				auto sp3 = sp2;
 				cout << *sp3 << "\n";
@@ -26,7 +25,7 @@ int main()
 				cout << "sp2.use_count() = " << sp2.use_count() << "\n";
 				cout << "sp3.use_count() = " << sp3.use_count() << "\n";
 				cout << "bir tusa basin ";
-				getchar();
+				(void)getchar();
 			}
 		}
 		cout << "sp1.use_count() = " << sp1.use_count() << "\n";
