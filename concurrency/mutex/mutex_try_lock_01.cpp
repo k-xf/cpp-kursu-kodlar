7187416180cae977588c98979bd1fba08fa09b85
Necati Ergin <46518309+necatiergin@users.noreply.github.com>
2022-05-06 13:32:54 +0300

    Update mutex_try_lock_01.cpp

--- concurrency/mutex/mutex_try_lock_01.cpp
+++ concurrency/mutex/mutex_try_lock_01.cpp
@@ -1,16 +1,23 @@
-// mutex::try_lock ornek kod
+/*
+	mutex::try_lock ornek kod
+	mutex sınıflarının try_lock fonksiyonları
+	mutex'i edinemezse ilgili thread'i bloke etmiyor. 
+	fonksiyon bu durumda false değer dönüyor.
+	böylece kilidi edinemediğinde / kilidi edinene kadar
+	başka işlemler yapabiliyoruz.
+*/
 
 #include <iostream>
 #include <thread>
 #include <mutex>
 
-int counter{};		// non-atomic counter
-std::mutex counter_mtx;				// locks access to counter
+int counter{};					// atomik olmayan paylaşılan değişken
+std::mutex counter_mtx;			// counter değişkenine erişim sağlayacak mutex
 
 void try_increase()
 {
 	for (int i = 0; i < 100'000; ++i) {
-		if (counter_mtx.try_lock()) {   // only increase if currently not locked:
+		if (counter_mtx.try_lock()) {   //sadece mutex edinilirse counter arttırılacak
 			++counter;
 			counter_mtx.unlock();
 		}
