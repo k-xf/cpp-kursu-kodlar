d13e27a0644f4adb4a86fa362504a1b645b1708e
Necati Ergin <46518309+necatiergin@users.noreply.github.com>
2022-05-12 18:55:12 +0300

    Create spinlock_mutex_01.cpp

---
+++ concurrency/atomic/spinlock_mutex_01.cpp
@@ -0,0 +1,53 @@
+#include <iostream>
+#include <thread>
+#include <atomic>
+#include <mutex>
+#include <vector>
+#include <iostream>
+
+class SpinLockMutex {
+public:
+	SpinLockMutex() 
+	{
+		m_f.clear(); 
+	}
+	void lock() 
+	{
+		while (m_f.test_and_set())
+			; //null statement
+	}
+
+	void unlock() 
+	{
+		m_f.clear();
+	}
+ private:
+	std::atomic_flag m_f;
+};
+
+SpinLockMutex mtx;
+unsigned long long counter{};
+
+void func() 
+{
+	for (int i{ 0 }; i < 100'000; ++i) {
+		mtx.lock();
+		++counter;
+		mtx.unlock();
+	}
+}
+
+int main()
+{
+	std::vector<std::thread> tvec;
+
+	for (int i = 0; i < 10; ++i) {
+		tvec.emplace_back(func);
+	}
+
+	for (auto &th : tvec) {
+		th.join();
+	}
+
+	std::cout << "counter = " << counter << "\n";
+}
