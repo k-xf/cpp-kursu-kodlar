e882cedfd208c81c4dc295ab95784ad253a27bdb
Necati Ergin <46518309+necatiergin@users.noreply.github.com>
2022-05-19 09:57:04 +0300

    Update atomic_flag_01.cpp

--- concurrency/atomic/atomic_flag_01.cpp
+++ concurrency/atomic/atomic_flag_01.cpp
@@ -8,6 +8,8 @@ int main()
 	cout << boolalpha;
 	// atomic_flag flag_x{ false }; //gecersiz
 	// atomic_flag flag_y{ true };  //gecersiz
+	atomic_flag flag_z; //C++ 17'de belirsiz deger C++20'de false değeri
+	cout << "flag_z = " << flag_z.test() << "\n"; //C++20
 	atomic_flag flag = ATOMIC_FLAG_INIT;  //gecerli
 	cout << "flag = " << flag.test() << "\n"; //C++20
 	auto b = flag.test_and_set();
@@ -17,7 +19,7 @@ int main()
 	cout << "flag = " << flag.test() << "\n";
 	b = flag.test_and_set();
 	cout << "b = " << b << "\n";
-	cout << "flag = " << flag.test() << "\n";  
+	cout << "flag = " << flag.test() << "\n";
 }
 
 
