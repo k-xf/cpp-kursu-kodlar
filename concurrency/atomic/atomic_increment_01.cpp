3129abca209565360023a9fe5619d726b24bc455
Necati Ergin <46518309+necatiergin@users.noreply.github.com>
2022-05-21 15:54:58 +0300

    Update atomic_increment_01.cpp

--- concurrency/atomic/atomic_increment_01.cpp
+++ concurrency/atomic/atomic_increment_01.cpp
@@ -4,9 +4,9 @@
 int main()
 {
 	std::atomic<int> x;
-	
+
 	int val = ++x; //atomic<T>::operator++()
 	std::cout << "val = " << val << "\n";
-	val = ++x; //atomic<T>::operator++(int)
+	val = x++; //atomic<T>::operator++(int)
 	std::cout << "val = " << val << "\n";
 }
