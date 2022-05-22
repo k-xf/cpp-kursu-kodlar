36f854786e804a58c9667d199832102a89846dac
Necati Ergin <46518309+necatiergin@users.noreply.github.com>
2022-05-22 07:57:56 +0300

    Update random_01.cpp

--- STL/random/random_01.cpp
+++ STL/random/random_01.cpp
@@ -1,13 +1,13 @@
 #include <random>
 #include <iostream>
 
-using namespace std;
-
 int main()
 {
+	using namespace std;
+
 	cout << typeid(mt19937).name() << "\n\n\n";
-	cout << typeid(mt19937_64).name() << \n\n\n";
-	
+	cout << typeid(mt19937_64).name() << "\n\n\n";
+
 	cout << "sizeof(mt19937)    = " << sizeof(mt19937) << "\n";
 	cout << "sizeof(mt19937_64) = " << sizeof(mt19937_64) << "\n";
 }
