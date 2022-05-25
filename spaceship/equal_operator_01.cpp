373f58f6f4194dcb3875eb8c06cc233d712c65ad
Necati Ergin <46518309+necatiergin@users.noreply.github.com>
2022-05-25 18:43:20 +0300

    Create equal_operator_01.cpp

---
+++ spaceship/equal_operator_01.cpp
@@ -0,0 +1,24 @@
+#include <iostream>
+
+struct Nec {
+public:
+	Nec(int val) : mx(val) {}
+	bool operator==(const Nec& other)const
+	{
+		return mx == other.mx;
+	}
+private:
+	int mx;
+};
+
+
+int main()
+{
+	std::cout << std::boolalpha;
+	Nec n1{ 43 }, n2{ 76 };
+
+	std::cout << (n1 == n2) << '\n';
+	std::cout << (n1 != n2) << '\n';
+	std::cout << (10 != n1) << '\n';
+	std::cout << (76 != n2) << '\n';
+}
