48d31d934ae9b067ad856fd2ae361ed3a1166fc6
Necati Ergin <46518309+necatiergin@users.noreply.github.com>
2022-05-29 10:38:33 +0300

    Create sm_04.cpp

---
+++ classes/static_members/sm_04.cpp
@@ -0,0 +1,35 @@
+#define _CRT_SECURE_NO_WARNINGS
+
+
+//nec.h
+class Nec {
+
+public:
+	inline static int x{ 10 };
+};
+
+//file a.cpp 
+//#include "nec.h"
+
+#include <iostream>
+void fa()
+{
+	std::cout << "&Nec::x = " << &Nec::x << '\n';
+}
+
+//file b.cpp 
+//#include "nec.h"
+void fb()
+{
+	std::cout << "&Nec::x = " << &Nec::x << '\n';
+}
+
+//main.cpp
+void fa();
+void fb();
+
+int main()
+{
+	fa();
+	fb();
+}
