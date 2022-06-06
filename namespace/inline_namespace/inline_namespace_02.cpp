f5a7adc4aef79e989996d209134bc4d095443491
Necati Ergin <46518309+necatiergin@users.noreply.github.com>
2022-06-06 18:50:49 +0300

    Create inline_namespace_02.cpp

---
+++ namespace/inline_namespace/inline_namespace_02.cpp
@@ -0,0 +1,28 @@
+namespace Nec {
+   namespace Nested {
+        class C {
+            //...
+        };
+        //...
+   }
+   using namespace Nested;
+   void func(Nested::C);
+}
+int main()
+{
+    Nec::Nested::C x;
+    func(x); //gecersiz
+}
+
+
+/*
+namespace Nec {
+   inline namespace Nested {
+        class C {
+            //...
+        };
+        //...
+   }
+   void func(Nested::C);
+}
+*/
