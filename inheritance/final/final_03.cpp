b372edd6416dc42f37c3e6c0cc2b1049f300d938
Necati Ergin <46518309+necatiergin@users.noreply.github.com>
2022-06-27 18:45:09 +0300

    Create final_03.cpp

---
+++ inheritance/final/final_03.cpp
@@ -0,0 +1,16 @@
+class Base {
+public:
+	virtual void func();
+	virtual ~Base() = default;
+};
+
+class Der : public Base {
+	//...
+public:
+	void func() final override;
+};
+
+class NecDer : public Der {  
+public:
+	virtual void func() override; //invalid
+};
