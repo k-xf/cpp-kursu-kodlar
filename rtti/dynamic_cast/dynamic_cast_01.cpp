1de9202c2a2b9d91f7cefee0cbff41e57b753fb8
Necati Ergin <46518309+necatiergin@users.noreply.github.com>
2022-06-29 16:53:57 +0300

    Create dynamic_cast_01.cpp

---
+++ rtti/dynamic_cast/dynamic_cast_01.cpp
@@ -0,0 +1,15 @@
+lass Base {
+public:
+};
+
+class Der : public Base {
+public:
+	void derfoo();
+};
+
+
+void func(Base* baseptr)
+{
+	if (auto pder = dynamic_cast<Der*>(baseptr)) //invalid
+		pder->derfoo();
+}
