4e3161b00ad0eb98b205761dc7988ab68015daca
Necati Ergin <46518309+necatiergin@users.noreply.github.com>
2022-06-30 14:43:55 +0300

    Update copy_backward_02.cpp

--- STL/algorithms/copy_move/copy_backward_02.cpp
+++ STL/algorithms/copy_move/copy_backward_02.cpp
@@ -2,15 +2,13 @@
 #include <algorithm>
 #include <iostream>
 
-
-using namespace std;
-
 int main()
 {
-	vector<int> ivec{ 10, 20, 30, 40, 50 };
-	vector<int> dest(ivec.size() * 2);
+	const std::vector<int> ivec{ 10, 20, 30, 40, 50 };
+	std::vector<int> dest(ivec.size() * 2);
 
 	copy_backward(ivec.begin(), ivec.end(), dest.end());
+	
 	for (auto i : dest)
 		std::cout << i << " ";
 }
