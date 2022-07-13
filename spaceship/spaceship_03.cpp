1eac91e4924ac6b776b998141cf89dbea78b1c53
Necati Ergin <46518309+necatiergin@users.noreply.github.com>
2022-07-13 11:12:55 +0300

    Update spaceship_03.cpp

--- spaceship/spaceship_03.cpp
+++ spaceship/spaceship_03.cpp
@@ -7,6 +7,8 @@ private:
 public:
 	constexpr Wrapper(int i)noexcept : m_id(i) { }
 	auto operator <=>(const Wrapper& other)const = default;	// C++20 spaceship operator
+	//friend auto operator <=>(const Wrapper& x, const Wrapper& other) = default;	// C++20 spaceship operator
+
 };
 
 int main()
@@ -16,9 +18,9 @@ int main()
 	std::cout << std::boolalpha;
 	std::cout << "x == y : " << (x == y) << "\n";   // false
 	std::cout << "x != y : " << (x != y) << "\n";   // true
-	std::cout << "x > y : " << (x > y) << "\n";     // false
+	std::cout << "x > y  : " << (x > y) << "\n";     // false
 	std::cout << "x >= y : " << (x >= y) << "\n";   // false
-	std::cout << "x < y : " << (x < y) << "\n";     // true
+	std::cout << "x < y  : " << (x < y) << "\n";     // true
 	std::cout << "x <= y : " << (x <= y) << "\n";   // true
 	std::cout << typeid(x <=> y).name() << '\n';  // struct std::strong_ordering
 }
