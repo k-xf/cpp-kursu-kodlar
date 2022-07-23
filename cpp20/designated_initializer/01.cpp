a32c36222b67bc92bb7b95fd5d1a058ae227d84e
Necati Ergin <46518309+necatiergin@users.noreply.github.com>
2022-07-23 21:47:02 +0300

    Create 01.cpp

---
+++ cpp20/designated_initializer/01.cpp
@@ -0,0 +1,18 @@
+#include <vector>
+
+struct Point
+{
+	double x, y, z;
+};
+
+int main()
+{
+	Point a;
+	a.x = 3.4;
+
+	Point p = { .x = 3, .y = 5, .z = 5 };
+	std::vector<Point> pvec;
+
+	pvec.push_back(Point{ .x = 1.1, .y = 2.2, .z = 3.3 });
+	pvec.push_back(Point{ .x = {1.1}, .y = {2.2}, .z = {3.3} });
+}
