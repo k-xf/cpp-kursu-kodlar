7c3e321a6c2e4ea0a26f46cd13503f515e874809
Necati Ergin <46518309+necatiergin@users.noreply.github.com>
2022-07-23 21:48:58 +0300

    Create 04.cpp

---
+++ cpp20/designated_initializer/04.cpp
@@ -0,0 +1,24 @@
+struct Time
+{
+	int min;
+	int hour;
+};
+
+struct Date
+{
+	int year;
+	int month;
+	int day;
+	Time time;
+	static int hmode;
+};
+
+int main()
+{
+	//Date d1 = { .hmode = 0 }; // invalid - static member
+	//Date d2 = { .month = 3, .year = 1998 }; // invalid - wrong order
+	//Date d3 = { 3, .year = 1998 }; // invalid - mix init
+	//Date d4 = {.time.min = 25; // invalid. Nested member access is now allowed.};
+
+	Date d5 = { .time = {32, 4} }; // valid
+}
