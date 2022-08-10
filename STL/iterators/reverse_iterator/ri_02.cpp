15b397392161a37ba02ceb18f9021439f1297b44
Necati Ergin <46518309+necatiergin@users.noreply.github.com>
2022-08-10 14:07:47 +0300

    Create ri_02.cpp

---
+++ STL/iterators/reverse_iterator/ri_02.cpp
@@ -0,0 +1,26 @@
+#include <vector>
+#include <iostream>
+#include <algorithm>
+
+int main()
+{
+	using namespace std;
+
+	vector<string> svec{ "metin", "ahmet", "nuri", "belgin", "samet","deniz" };
+
+	if (auto iter = find_if(svec.begin(), svec.end(), [](const string& s) {return s.contains('a'); }); iter != svec.end()) {
+		std::cout << "found... " << *iter << "\n";
+	}
+	else {
+		std::cout << "bulunamadi\n";
+	}
+
+
+	if (auto riter = find_if(svec.rbegin(), svec.rend(), [](const string& s) {return s.contains('a'); }); riter != svec.rend()) {
+		std::cout << "found... " << *riter << "\n";
+	}
+	else {
+		std::cout << "bulunamadi\n";
+	}
+
+}
