06d1798075765687ba11925072852dbde81450e6
Necati Ergin <46518309+necatiergin@users.noreply.github.com>
2022-09-19 15:46:48 +0300

    Update string_ctor_01.cpp

--- STL/bitset/string_ctor_01.cpp
+++ STL/bitset/string_ctor_01.cpp
@@ -8,9 +8,9 @@ int main()
 	string str{ "11111111111111111" };
 
 	bitset<32> bs1{ str };
-	cout << bs1 << "\n";
-	bitset<32> bs2{ str, 8};
-	cout << bs1 << "\n";
+	cout << bs1 << '\n';
+	bitset<32> bs2{ str, 8 };
+	cout << bs1 << '\n';
 	bitset<32> bs3{ str, 8, 3 };
-	cout << bs3 << "\n";
+	cout << bs3 << '\n';
 }
