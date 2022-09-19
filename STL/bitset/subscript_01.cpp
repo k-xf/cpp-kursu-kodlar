0572e8b22fca4903bed1e3c66ab1db17ce80c093
Necati Ergin <46518309+necatiergin@users.noreply.github.com>
2022-09-19 15:46:05 +0300

    Update subscript_01.cpp

--- STL/bitset/subscript_01.cpp
+++ STL/bitset/subscript_01.cpp
@@ -10,8 +10,8 @@ int main()
 	cin >> uval;
 	bitset<32> bs = uval;
 
-	cout << bs[10].operator bool() << "\n";
-	cout << bs[10] << "\n";
+	cout << bs[10].operator bool() << '\n';
+	cout << bs[10] << '\n';
 	bs[5] = bs[2];
 	bs[4].flip();
 	bs[7] = ~bs[2];
