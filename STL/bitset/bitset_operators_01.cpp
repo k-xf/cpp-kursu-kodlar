e49a7dc71d26b3ee97e33da76e4e845777f65781
Necati Ergin <46518309+necatiergin@users.noreply.github.com>
2022-09-19 15:45:16 +0300

    Update bitset_operators_01.cpp

--- STL/bitset/bitset_operators_01.cpp
+++ STL/bitset/bitset_operators_01.cpp
@@ -5,11 +5,11 @@ int main()
 {
 	using namespace std;
 
-	bitset<16> bs{65535};
-	cout << bs << "\n";
+	bitset<16> bs{ 65535 };
+	cout << bs << '\n';
 	bs >>= 5;
-	cout << bs << "\n";
+	cout << bs << '\n';
 	bs = 65535;
-	cout << (bs << 2) << "\n";
-	cout << (bs >> 3) << "\n";
+	cout << (bs << 2) << '\n';
+	cout << (bs >> 3) << '\n';
 }
