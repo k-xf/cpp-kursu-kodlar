0aab92e60816c61d469218afbbf0810efae12b27
Necati Ergin <46518309+necatiergin@users.noreply.github.com>
2022-09-22 13:29:43 +0300

    Create sb_03.cpp

---
+++ structured_binding/sb_03.cpp
@@ -0,0 +1,26 @@
+#include <iostream>
+
+class A {
+public:
+	A() = default;
+	//...
+	int a{}, b{};
+	friend void f2();
+private:
+	int c{};
+};
+
+void f1()
+{
+	A ax;
+
+	//auto [x, y, z] = ax; //invalid
+}
+
+void f2()
+{
+	A ax;
+
+	auto [x, y, z] = ax; 
+	//it is invalid in C++17 but valid in C++20
+}
