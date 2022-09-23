a2915304b14a048c97341ffbff8159505f72b951
Necati Ergin <46518309+necatiergin@users.noreply.github.com>
2022-09-23 18:40:05 +0300

    Create pm_01.cpp

---
+++ pointer_to_members/pm_01.cpp
@@ -0,0 +1,17 @@
+#include <iostream>
+
+class Nec {
+public:
+	void foo();
+	void bar()const;
+};
+
+
+int main()
+{
+	auto fpx = &Nec::foo;
+	void (Nec::*fpx_)() = &Nec::foo;
+
+	auto fpy = &Nec::bar;
+	void (Nec::*fpy_)()const = &Nec::bar;
+}
