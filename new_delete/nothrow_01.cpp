f3f3cb9f8411253b96abc60b41fed257d53b0d85
Necati Ergin <46518309+necatiergin@users.noreply.github.com>
2022-09-27 11:06:15 +0300

    Create nothrow_01.cpp

---
+++ new_delete/nothrow_01.cpp
@@ -0,0 +1,27 @@
+#include <vector>
+#include <iostream>
+
+struct Nec {
+	unsigned char buffer[1024 * 1024 * 16];
+};
+
+std::vector<Nec*> pvec{};
+
+int main()
+{
+	std::cout << "sizeof(Nec) = " << sizeof(Nec) << '\n';
+	int cnt{};
+
+	for (;;) {
+		auto p = new(std::nothrow)Nec;
+		if (!p) {
+			std::cout << "cannot allocate memory for Nec object!\n";
+			break;
+		}
+		pvec.push_back(p);
+		std::cout << ++cnt << '\n';
+	}
+
+	for (auto p : pvec)
+		delete p;
+}
