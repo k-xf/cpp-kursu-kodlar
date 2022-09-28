121e881fb04f9b14a1d4f9415817220b6dd2cdd4
Necati Ergin <46518309+necatiergin@users.noreply.github.com>
2022-09-28 18:26:57 +0300

    Update member_selection_ops.cpp

--- STL/unique_ptr/members/member_selection_ops.cpp
+++ STL/unique_ptr/members/member_selection_ops.cpp
@@ -11,12 +11,12 @@ int main()
 	{
 		unique_ptr<Triple> upx{ new Triple{10, 20, 30} };
 
-		cout << (upx ? "dolu" : "bos") << "\n";
-		cout << *upx << "\n";
+		cout << (upx ? "not empty" : "empty") << "\n";
+		cout << *upx << '\n';
 		upx->set(3, 6, 7);
-		cout << *upx << "\n";
-		cout << "blok sonu\n";
+		cout << *upx << '\n';
+		cout << "the end of the nested block\n";
 	}
 
-	cout << "main devam ediyor\n";
+	cout << "main goes on\n";
 }
