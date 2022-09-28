5c0cc877b8fc50b7d8eff70201dcd8ec87a57bde
Necati Ergin <46518309+necatiergin@users.noreply.github.com>
2022-09-28 18:28:33 +0300

    Update reset_05.cpp

--- STL/unique_ptr/members/reset_05.cpp
+++ STL/unique_ptr/members/reset_05.cpp
@@ -6,10 +6,10 @@ int main()
 	using namespace std;
 
 	auto upx = make_unique<Triple>(10, 30, 56);
-	cout << *upx << "\n";
-	cout << (upx ? "dolu" : "bos") << "\n";
+	cout << *upx << '\n';
+	cout << (upx ? "not empty" : "empty") << '\n';
 
 	upx.reset(new Triple{ -1, -2, -3 });
 	(void)getchar();
-	cout << (upx ? "dolu" : "bos") << "\n";
+	cout << (upx ? "not empty" : "empty") << '\n';
 }
