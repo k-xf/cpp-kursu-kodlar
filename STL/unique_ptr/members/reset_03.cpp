dc605b3e0dfb0781a85c881f2c0e5eecb07db113
Necati Ergin <46518309+necatiergin@users.noreply.github.com>
2022-09-28 18:25:45 +0300

    Update reset_03.cpp

--- STL/unique_ptr/members/reset_03.cpp
+++ STL/unique_ptr/members/reset_03.cpp
@@ -4,10 +4,10 @@
 int main()
 {
 	auto upx = std::make_unique<Triple>(10, 30, 56);
-	std::cout << *upx << "\n";
-	std::cout << (upx ? "dolu" : "bos") << "\n";
+	std::cout << *upx << '\n';
+	std::cout << (upx ? "not empty" : "empty") << '\n';
 	upx.reset();
 	//upx.reset(nullptr);
-	std::cout << (upx ? "dolu" : "bos") << "\n";
-	std::cout << "main devam ediyor\n";
+	std::cout << (upx ? "not empty" : "empty") << '\n';
+	std::cout << "main goes on\n";
 }
