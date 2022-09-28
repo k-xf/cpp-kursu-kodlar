7b8cc1fbcc6263588951e711f32a0e907774cd02
Necati Ergin <46518309+necatiergin@users.noreply.github.com>
2022-09-28 18:41:31 +0300

    Update move_02.cpp

--- STL/unique_ptr/move_semantics/move_02.cpp
+++ STL/unique_ptr/move_semantics/move_02.cpp
@@ -9,13 +9,13 @@ int main()
 	auto upx = make_unique<Triple>(10, 30, 56);
 	auto upy = make_unique<Triple>(-6, -1, -20);
 
-	cout << "upx = " << (upx ? "dolu" : "bos") << "\n";
-	cout << "upy = " << (upy ? "dolu" : "bos") << "\n";
-	cout << "*upx = " << *upx << "\n";
-	cout << "*upy = " << *upy << "\n";
+	cout << "upx = " << (upx ? "not empty" : "empty") << '\n';
+	cout << "upy = " << (upy ? "not empty" : "empty") << '\n';
+	cout << "*upx = " << *upx << '\n';
+	cout << "*upy = " << *upy << '\n';
 	(void) getchar();
 	upx = move(upy);
-	cout << "upx = " << (upx ? "dolu" : "bos") << "\n";
-	cout << "upy = " << (upy ? "dolu" : "bos") << "\n";
+	cout << "upx = " << (upx ? "not empty" : "empty") << '\n';
+	cout << "upy = " << (upy ? "not empty" : "empty") << '\n';
 	(void) getchar();
 }
