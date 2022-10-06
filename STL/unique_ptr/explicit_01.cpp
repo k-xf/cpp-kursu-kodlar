d7d0dc15404591810505838533d9a3d953630765
Necati Ergin <46518309+necatiergin@users.noreply.github.com>
2022-10-06 09:39:51 +0300

    Update explicit_01.cpp

--- STL/unique_ptr/explicit_01.cpp
+++ STL/unique_ptr/explicit_01.cpp
@@ -1,9 +1,21 @@
 //constructor with T* param is explicit
 
 #include <memory>
-#include "triple.h"
+#include <string>
 
-std::unique_ptr<Triple> foo()
+std::unique_ptr<std::string> foo()
 {
-	return new Triple; //invalid
+	return new std::string{"necati"}; //invalid
+}
+
+void bar(std::unique_ptr<std::string>);
+
+int main()
+{
+	using namespace std;
+
+	//unique_ptr<string> ux = new string; //invalid
+	unique_ptr<string> ux(new string); //valid
+	//bar(new string); //invalid
+	bar(unique_ptr<string>(new string)); //valid
 }
