e403df69183589e50517e3b80aff3e3c8e7bb9a4
Necati Ergin <46518309+necatiergin@users.noreply.github.com>
2022-10-06 09:29:53 +0300

    Update make_unique_01.cpp

--- STL/unique_ptr/make_unique/make_unique_01.cpp
+++ STL/unique_ptr/make_unique/make_unique_01.cpp
@@ -1,5 +1,5 @@
 #include <memory>
-#include "triple.h"
+#include "point.h"
 #include <iostream>
 
 template<typename T, typename ...Args>
@@ -11,7 +11,7 @@ std::unique_ptr<T> MakeUnique(Args && ...args)
 int main()
 {
 	std::cout << "main basladi\n";
-	auto uptr = MakeUnique<Triple>(10, 20, 30);
+	auto uptr = MakeUnique<Point>(10, 20, 30);
 
 	std::cout << *uptr << "\n";
 	uptr->set(3, 4, 7);
