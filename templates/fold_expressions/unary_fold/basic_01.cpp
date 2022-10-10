4210430a2692744da1b765ffe66cc4273b740d40
Necati Ergin <46518309+necatiergin@users.noreply.github.com>
2022-10-10 19:08:23 +0300

    Update and rename templates/fold_expressions/basic_01.cpp to templates/fold_expressions/unary_fold/basic_01.cpp

--- templates/fold_expressions/unary_fold/basic_01.cpp
+++ templates/fold_expressions/unary_fold/basic_01.cpp
@@ -2,24 +2,24 @@
 #include <iostream>
 
 template<typename... T>
-auto sum_left(T... args) 
+auto sum_left(T... args)
 {
-	return (... + args);
+	return (... + args);  //unary left fold
 }
 
 template<typename... T>
-auto sum_right(T... args) 
+auto sum_right(T... args)
 {
-	return (args + ...);
+	return (args + ...);  //unary right fold
 }
 
 int main()
 {
 	using namespace std::literals;
 	auto x = sum_left(1, 3, 5, 7, 9);
-	std::cout << "x = " << x << "\n";
+	std::cout << "x = " << x << '\n';
 	auto y = sum_right(1, 3, 5, 7, 9);
-	std::cout << "y = " << y << "\n";
+	std::cout << "y = " << y << '\n';
 
 	auto sx = sum_left("ali"s, "can", " usta", "oglu");
 	std::cout << "sx = " << sx << "\n";
