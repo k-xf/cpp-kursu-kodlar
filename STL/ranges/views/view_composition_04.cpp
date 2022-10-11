24198a7631b4a8b6f8ef3086b60d01aae0dc84d4
Necati Ergin <46518309+necatiergin@users.noreply.github.com>
2022-10-11 13:30:37 +0300

    Create view_composition_04.cpp

---
+++ STL/ranges/views/view_composition_04.cpp
@@ -0,0 +1,13 @@
+#include <iostream>
+#include <ranges>
+
+int main()
+{
+	using namespace std;
+	
+	auto v = views::iota(10);
+	for (auto x : views::iota(10)  
+		          | views::transform([](int x) { return x + 10; })
+		          | views::take(20)) 
+		cout << x << ' '; // 20 21 22 23 24 25 26 27 28 29 30 31 32 33 34 35 36 37 38 39
+}
