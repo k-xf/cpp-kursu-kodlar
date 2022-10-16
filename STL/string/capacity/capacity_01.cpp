c61bbd744733abd03b0aff7c608a2e3098757d24
Necati Ergin <46518309+necatiergin@users.noreply.github.com>
2022-10-16 08:55:08 +0300

    Update and rename STL/string/capacity_02.cpp to STL/string/capacity/capacity_01.cpp

--- STL/string/capacity/capacity_01.cpp
+++ STL/string/capacity/capacity_01.cpp
@@ -3,19 +3,16 @@
 
 int main()
 {
-	using namespace std;
-
-	string s{ "aaaaaaa" };
+	std::string s{ "aaaaaaa" };
 	auto cap = s.capacity();
 	int alloc_cnt{};
-	
+
 	for (int i = 0; i < 1'000'000; ++i) {
 		s.push_back('a');
 		auto new_cap = s.capacity();
 		if (new_cap > cap) {
-			std::cout << ++alloc_cnt << ". reallocation size : " << s.size() << "  capacity : " << s.capacity() << "\n";
+			std::cout << ++alloc_cnt << ". reallocation size : " << s.size() << "  capacity : " << s.capacity() << '\n';
 			cap = new_cap;
 		}
 	}
 }
-
