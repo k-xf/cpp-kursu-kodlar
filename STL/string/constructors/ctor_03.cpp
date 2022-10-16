a2309867a2784f7e800f7b61e6246c4a33dbfcfa
Necati Ergin <46518309+necatiergin@users.noreply.github.com>
2022-10-16 08:47:25 +0300

    Update and rename STL/string/constructor_03.cpp to STL/string/constructors/ctor_03.cpp

--- STL/string/constructors/ctor_03.cpp
+++ STL/string/constructors/ctor_03.cpp
@@ -1,20 +1,17 @@
 #include <string>
 #include <iostream>
 
-void print(const std::string &str)
+void print(const std::string& str)
 {
 	std::cout << "[" << str << "] [" << str.size() << "/" << str.capacity() << "]\n";
 }
 
 int main()
 {
-	using namespace std;
-
-	//string s1('a'); //gecersiz char parametreli ctor yok
-	string s2(65, 'A'); //fill ctor
+	//std::string s1('a'); //gecersiz char parametreli ctor yok
+	std::string s2(65, 'A'); //fill ctor
 	print(s2);
-	string s3{ 65, 'A' }; //init_list ctor
+	std::string s3{ 65, 'A' }; //init_list ctor
 	//string s3 = { 65, 'A' }; //init_list ctor
 	print(s3);
 }
-
