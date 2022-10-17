96b5dfd0d8bdb04c8d26363d665cbc24cacfeeef
Necati Ergin <46518309+necatiergin@users.noreply.github.com>
2022-10-17 15:42:36 +0300

    Update and rename iostream/formatting/boolalpha_01.cpp to iostream/formatting/basic/basic_04..cpp

--- iostream/formatting/basic/basic_04..cpp
+++ iostream/formatting/basic/basic_04..cpp
@@ -4,11 +4,11 @@ int main()
 {
 	using namespace std;
 
-	cout << (10 < 22) << "\n";
+	cout << (10 < 22) << '\n';
 	cout.setf(ios::boolalpha);
 	//cout.flags(cout.flags() | ios::boolalpha);
-	cout << (10 < 22) << "\n";
+	cout << (10 < 22) << '\n';
 	cout.unsetf(ios::boolalpha);
 	//cout.flags(cout.flags() & ~ios::boolalpha);
-	cout << (10 < 22) << "\n";
+	cout << (10 < 22) << '\n';
 }
