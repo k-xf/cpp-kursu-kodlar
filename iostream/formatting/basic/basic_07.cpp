f39e26a630275283b4285ed37f46986d73e70cc6
Necati Ergin <46518309+necatiergin@users.noreply.github.com>
2022-10-17 15:45:48 +0300

    Update and rename iostream/formatting/setfill_01.cpp to iostream/formatting/basic/basic_07.cpp

--- iostream/formatting/basic/basic_07.cpp
+++ iostream/formatting/basic/basic_07.cpp
@@ -6,5 +6,5 @@ int main()
 	using namespace std;
 
 	int ival = 3451;
-	cout << setfill('*') << setw(12) << ival << "necati" << "\n";
+	cout << setfill('*') << setw(12) << ival << "necati" << '\n';
 }
