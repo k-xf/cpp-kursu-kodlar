b36b74743c98ec180d6581b8843ad33091766ed0
Necati Ergin <46518309+necatiergin@users.noreply.github.com>
2022-10-26 15:24:23 +0300

    Update and rename iostream/fstream/unformatted_02.cpp to 2840_IOSTREAM_FSTREAM/unformatted_02.cpp

--- 2840_IOSTREAM_FSTREAM/unformatted_02.cpp
+++ 2840_IOSTREAM_FSTREAM/unformatted_02.cpp
@@ -8,17 +8,21 @@
 int main()
 {
 	using namespace std;
-	constexpr size_t size = 100;
+	constexpr size_t size = 100u;
+
 	ifstream ifs{ "primes.dat", ios::binary };
 	if (ifs.fail()) {
 		cerr << "dosya acilamadi\n";
 		return 1;
 	}
 	vector<int> ivec(size);
-	
-	while (ifs.read(reinterpret_cast<char*>(ivec.data()), sizeof(int) * 100)) {
-		auto n = static_cast<int>(ifs.gcount() / sizeof(int));
+
+	while (ifs.read(reinterpret_cast<char*>(ivec.data()), sizeof(int) * size)) {
+		copy(ivec.begin(), ivec.begin() + size, ostream_iterator<int>(cout, " "));
+		(void)getchar();
+	}
+
+	if (auto n = static_cast<int>(ifs.gcount() / sizeof(int))) {
 		copy(ivec.begin(), ivec.begin() + n, ostream_iterator<int>(cout, " "));
-		cout << "\ndevam etmek icin bir tusa basin "; static_cast<void>(getchar());
 	}
 }
