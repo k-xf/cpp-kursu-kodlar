ec75681d6870d60ea24a158dde043ac9734a79ce
Necati Ergin <46518309+necatiergin@users.noreply.github.com>
2022-10-31 16:45:39 +0300

    Update and rename STL/random/random_01.cpp to 2910_RANDOM/engine_sizeof_01.cpp

--- 2910_RANDOM/engine_sizeof_01.cpp
+++ 2910_RANDOM/engine_sizeof_01.cpp
@@ -8,6 +8,6 @@ int main()
 	cout << typeid(mt19937).name() << "\n\n\n";
 	cout << typeid(mt19937_64).name() << "\n\n\n";
 
-	cout << "sizeof(mt19937)    = " << sizeof(mt19937) << "\n";
-	cout << "sizeof(mt19937_64) = " << sizeof(mt19937_64) << "\n";
+	cout << "sizeof(mt19937)    = " << sizeof(mt19937) << '\n';
+	cout << "sizeof(mt19937_64) = " << sizeof(mt19937_64) << '\n';
 }
