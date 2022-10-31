6fb2e9c7b4159f8db6e0f9cd99c913f1d88c0e9d
Necati Ergin <46518309+necatiergin@users.noreply.github.com>
2022-10-31 16:38:03 +0300

    Update distribution_01.cpp

--- 2910_RANDOM/distribution/distribution_01.cpp
+++ 2910_RANDOM/distribution/distribution_01.cpp
@@ -1,12 +1,14 @@
 #include <random>
 #include <iostream>
 
-using namespace std;
-
 int main()
 {
+	using namespace std;
+	
 	mt19937 eng{ random_device{}() };
-	uniform_int_distribution<>dist{ 1940, 2019 };
+	//uniform_int_distribution<int>dist{ 1940, 2019 };
+	//uniform_int_distribution<>dist{ 1940, 2019 };
+	uniform_int_distribution dist{ 1940, 2019 }; //CTAD
 
 	for (int i = 0; i < 100; ++i)
 		cout << dist(eng) << " ";
