26df4f7b27c6e3b70bb9334b5c0b477678502028
Necati Ergin <46518309+necatiergin@users.noreply.github.com>
2022-11-02 18:06:48 +0300

    Update duration_07.cpp

--- 2900_CHRONO/duration_07.cpp
+++ 2900_CHRONO/duration_07.cpp
@@ -1,7 +1,6 @@
 #include <chrono>
 #include <iostream>
 
-
 using namespace std;
 using namespace chrono;
 
@@ -10,17 +9,16 @@ using dsec = duration<double>;
 int main()
 {
 	microseconds us{ 7654777 };
-	nanoseconds nx = us;  //daha ince (fine) bir türden daha kaba (coarese) türe otomatik dönüşüm
+	nanoseconds nx = us;  //daha ince (fine) bir türden daha kaba (coarse) türe otomatik dönüşüm
 
 	//milliseconds msx = us;  //gecersiz daha ince türe otomatik dönüşüm yok
 	milliseconds msx = duration_cast<milliseconds>(us);
-	cout << msx.count() << "\n";
+	cout << msx.count() << '\n';
 	msx = ceil<milliseconds>(us);
-	cout << msx.count() << "\n";
+	cout << msx.count() << '\n';
 	msx = floor<milliseconds>(us);
 	msx = round<milliseconds>(us);
-	cout << msx.count() << "\n";
+	cout << msx.count() << '\n';
 	dsec ds = us;
-	cout << ds.count() << "\n";
-
+	cout << ds.count() << '\n';
 }
