26ae913c2811b39a4fab15d8eb605a68556c09bf
Necati Ergin <46518309+necatiergin@users.noreply.github.com>
2022-11-02 17:14:36 +0300

    Update time_point_01.cpp

--- 2900_CHRONO/time_point_01.cpp
+++ 2900_CHRONO/time_point_01.cpp
@@ -1,27 +1,28 @@
-#include <iostream>
 #include <chrono>
 #include <vector>
-#include "nutility.h"
 #include <random>
 #include <algorithm>
+#include <iostream>
 
 using namespace std;
 using namespace chrono;
 
 int main()
 {
-	vector<int> ivec;
+	constexpr size_t size{ 1'000'000 };
+	vector<int> ivec(size);
+	
 	mt19937 eng;
-	uniform_int_distribution<> dist{ 0, 1000000 };
-	cout << "islem basliyor\n";
-	auto tp_start = steady_clock::now();
+	uniform_int_distribution dist{ 0, 1'000'000 };
+	
+	const auto tp_start = steady_clock::now();
 
-	fc(ivec, 10'000'000, [&] {return dist(eng); });
+	generate(ivec.begin(), ivec.end(), [&] {return dist(eng); });
 	sort(ivec.begin(), ivec.end());
 
-	auto tp_end = steady_clock::now();
+	const auto tp_end = steady_clock::now();
 	cout << "islem sona erdi\n";
 
-	cout << "sure : " << duration_cast<milliseconds>(tp_end - tp_start).count() << " milisaniye \n";
-	cout << "sure : " << duration<double>(tp_end - tp_start).count() << " saniye \n";
+	cout << "sure : " << duration_cast<milliseconds>(tp_end - tp_start).count() << " milliseconds\n";
+	cout << "sure : " << duration<double>(tp_end - tp_start).count() << " seconds\n";
 }
