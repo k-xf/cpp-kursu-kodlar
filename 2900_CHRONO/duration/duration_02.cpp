60c14da8b720cb695be16b799a5bead4a638a881
Necati Ergin <46518309+necatiergin@users.noreply.github.com>
2022-11-02 17:57:59 +0300

    Update and rename 2900_CHRONO/duration_02.cpp to 2900_CHRONO/duration/duration_02.cpp

--- 2900_CHRONO/duration/duration_02.cpp
+++ 2900_CHRONO/duration/duration_02.cpp
@@ -12,7 +12,7 @@ int main()
 	std::cout << "s3.count() = " << s3.count() << "\n";
 	seconds s4{};
 	std::cout << "s4.count() = " << s4.count() << "\n";
-	seconds s5;
+	//seconds s5;
 	//std::cout << "s5.count() = " << s5.count() << "\n"; //ub
 	seconds s6 = 675s;
 	std::cout << "s6.count() = " << s6.count() << "\n"; //ub
