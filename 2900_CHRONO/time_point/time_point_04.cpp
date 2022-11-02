49f8187af52f239458ad92c71a073e82e0753e28
Necati Ergin <46518309+necatiergin@users.noreply.github.com>
2022-11-02 17:29:22 +0300

    Update and rename 2900_CHRONO/time_point_04.cpp to 2900_CHRONO/time_point/time_point_04.cpp

--- 2900_CHRONO/time_point/time_point_04.cpp
+++ 2900_CHRONO/time_point/time_point_04.cpp
@@ -4,9 +4,9 @@
 using namespace std;
 using namespace chrono;
 
-
 int main()
 {
 	mt19937 eng(static_cast<unsigned>(duration_cast<milliseconds>(system_clock::now().time_since_epoch()).count()));
+        //...
 }
 
