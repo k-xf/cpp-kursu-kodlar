65643d4212a125d2707f532b774ce3291bf76b75
Necati Ergin <46518309+necatiergin@users.noreply.github.com>
2022-11-09 19:04:15 +0300

    Update value_or_02.cpp

--- 2920_OPTIONAL/value_or_02.cpp
+++ 2920_OPTIONAL/value_or_02.cpp
@@ -5,7 +5,7 @@ int main()
 	std::optional<int> op{ 10 };
 
 	op.value() = 20;
-	op.value_or(0) = 30; //gecersiz
-	//value_or islevi referans döndürmüyor.
+	op.value_or(0) = 30; //invalid
+	//value_or member function doesn't return a reference.
 	//...
 }
