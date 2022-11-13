94f7e2aed53b2b8f95c42388edff0f6452af8b97
Necati Ergin <46518309+necatiergin@users.noreply.github.com>
2022-11-13 08:40:56 +0300

    Create terminate_03.cpp

---
+++ 0600_EXCEPTION_HANDLING/terminate/terminate_03.cpp
@@ -0,0 +1,29 @@
+#include <iostream>
+#include <exception>
+
+
+class Nec {
+public:
+	Nec(int x)
+	{
+		if (x % 5 == 0)
+			throw 1;
+		//...
+	}
+};
+
+
+void myterminate()
+{
+	std::cout << "myterminate\n";
+	std::exit(EXIT_FAILURE);
+}
+
+auto f = std::set_terminate(myterminate);
+
+Nec gnec{ 25 };
+
+int main()
+{
+
+}
