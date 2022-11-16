7e7906ecd0d26169fe25515f01b9c82133e19f75
Necati Ergin <46518309+necatiergin@users.noreply.github.com>
2022-11-16 19:05:30 +0300

    Update any_02.cpp

--- 2940_ANY/any_02.cpp
+++ 2940_ANY/any_02.cpp
@@ -5,16 +5,16 @@
 int main()
 {
 	using namespace std;
-	using namespace std::literals;
+
 	cout << boolalpha;
 
 	any ax = 10;
-	std::cout << ax.type().name() << ": " << any_cast<int>(ax) << '\n';
+	cout << ax.type().name() << ": " << any_cast<int>(ax) << '\n';
 	ax = 2.781828;
 	cout << ax.type().name() << ": " << any_cast<double>(ax) << '\n';
 	ax = true;
 	cout << ax.type().name() << ": " << any_cast<bool>(ax) << '\n';
-	
+
 	ax = "alican"s;
 	cout << ax.type().name() << ": " << any_cast<string>(ax) << '\n';
 
@@ -24,7 +24,7 @@ int main()
 		ax = 21;
 		cout << any_cast<float>(ax) << '\n';
 	}
-	catch (const std::bad_any_cast& e)
+	catch (const bad_any_cast& e)
 	{
 		std::cout << "hata yakalandi: " << e.what() << '\n';
 	}
@@ -33,19 +33,19 @@ int main()
 	ax = 1;
 	if (ax.has_value()) {
 		cout << "tur : " << ax.type().name() <<
-		" deger : " << any_cast<int>(ax) << "\n";
+			" deger : " << any_cast<int>(ax) << "\n";
 	}
 
 	// reset
 	ax.reset();
 	if (!ax.has_value())
 	{
-		std::cout << "degere sahip degil\n";
+		cout << "degere sahip degil\n";
 	}
 
 	// tutulan degere pointer
 	ax = 1;
-	//int* iptr = std::any_cast<int>(&ax);
-	auto iptr = std::any_cast<int>(&ax);
-	std::cout << *iptr << "\n";
+	//int* iptr = any_cast<int>(&ax);
+	auto iptr = any_cast<int>(&ax);
+	cout << *iptr << '\n';
 }
