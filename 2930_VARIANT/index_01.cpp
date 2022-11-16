b8106efd801b96adbe959ec4144771df6cdb8c67
Necati Ergin <46518309+necatiergin@users.noreply.github.com>
2022-11-16 14:52:02 +0300

    Update index_01.cpp

--- 2930_VARIANT/index_01.cpp
+++ 2930_VARIANT/index_01.cpp
@@ -4,14 +4,14 @@
 #include <string>
 #include <iostream>
 
-using namespace std::literals;
-
 int main()
 {
-	std::variant<int, std::string, double> var;  //default constructed, holds int, index = 0;
-	std::cout << "index = " << var.index() << "\n";
+	using namespace std;
+
+	variant<int, string, double> var;  //default constructed, holds int, index = 0;
+	cout << "index = " << var.index() << '\n';
 	var = "neco"s;
-	std::cout << "index = " << var.index() << "\n";
+	std::cout << "index = " << var.index() << '\n';
 	var = 2.3;
-	std::cout << "index = " << var.index() << "\n";
+	std::cout << "index = " << var.index() << '\n';
 }
