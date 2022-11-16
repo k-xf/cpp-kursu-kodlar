2b9eca1247036d7c878b0a9f621a7ee90fb7c902
Necati Ergin <46518309+necatiergin@users.noreply.github.com>
2022-11-16 17:20:42 +0300

    Update in_place_index.cpp

--- 2930_VARIANT/in_place_index.cpp
+++ 2930_VARIANT/in_place_index.cpp
@@ -4,15 +4,17 @@
 
 int main()
 {
+	using namespace std;
+
 	auto pred = [](int x, int y) {
-		return std::abs(x) < std::abs(y);
+		return abs(x) < abs(y);
 	};
-	
+
 	std::variant<
-		std::set<int>, 
-		std::set<int, decltype(pred)>> vx{ 
+		set<int>,
+		set<int, decltype(pred)>> vx{
 		std::in_place_index<1>,
 		{4, 8, -7, -2, 0, 5},
-		pred};
+		pred };
 
 }
