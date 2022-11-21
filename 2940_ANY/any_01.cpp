df45f58f464344842d3c37be19e502da074174d7
Necati Ergin <46518309+necatiergin@users.noreply.github.com>
2022-11-21 17:05:40 +0300

    Update any_01.cpp

--- 2940_ANY/any_01.cpp
+++ 2940_ANY/any_01.cpp
@@ -13,7 +13,7 @@ int main()
 	any a4{ "necati"s }; //std::string
 	any a5{ std::bitset<16>{} }; //std::bitset<16>
 	any a6{ std::vector<int>{1, 3, 5} }; //std::vector<int>
-	any b1;  //boş
-	any b2{}; //boş
-	any b3 = {}; //boş
+	any b1;  //empty
+	any b2{}; //empty
+	any b3 = {}; //empty
 }
