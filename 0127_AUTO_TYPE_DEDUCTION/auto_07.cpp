2795311a98cd95a1cab4460b61157a8b25ba6363
Necati Ergin <46518309+necatiergin@users.noreply.github.com>
2022-11-22 11:24:37 +0300

    Update auto_07.cpp

--- 0127_AUTO_TYPE_DEDUCTION/auto_07.cpp
+++ 0127_AUTO_TYPE_DEDUCTION/auto_07.cpp
@@ -1,1 +1,14 @@
+int main()
+{
+	auto &&r1 = 10;
+	//int &&r1
 
+	//type for auto is int
+	// declaration type of r1 is int &&
+	int x = 23;
+	auto&& r2 = x;
+
+	// type for auto is int&
+	// int& && reference collapsing to int & type
+	// declaration type of r2 is int &
+}
